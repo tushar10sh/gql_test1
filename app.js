6c180e582f409cc81486aae474f649141628e083
Tushar <tushar.10sh@gmail.com>
2019-11-08 20:58:00 +0530

    added author support and graph connected

--- app.js
+++ app.js
@@ -39,8 +39,35 @@ const BookType = new GraphQLObjectType({
         },
         authorId: {
             type: GraphQLNonNull(GraphQLInt)
+        },
+        author: {
+            type: AuthorType,
+            resolve: (book) => {
+                return authors.find( (author) => author.id === book.authorId)
+            }
         }
     })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: "Author",
+    description: "This represents an Author",
+    fields: {
+        id: {
+            type: GraphQLNonNull(GraphQLInt)
+        },
+        name: {
+            type: GraphQLNonNull(GraphQLString)
+        },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: (author) => {
+                return books.filter( (book) => {
+                    return book.authorId === author.id
+                })
+            } 
+        }
+    }
 });
 
 const  RootQueryType = new GraphQLObjectType({
@@ -51,6 +78,11 @@ const  RootQueryType = new GraphQLObjectType({
             type: new GraphQLList(BookType),
             description: "List of Books",
             resolve: () => books
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            description: "List of Authors",
+            resolve: () => authors
         }
     })
 });
